e5b8220d6026a0571539d2192d19ba29691c45e8
Gonzaemon111 <gonzaemon.engineer@gmail.com>
2023-07-18 01:26:43 +0900

    微修正

--- src/components/organisms/Task/Tables/index.tsx
+++ src/components/organisms/Task/Tables/index.tsx
@@ -35,7 +35,10 @@ export function TaskTable({ tasks }: Props) {
         <tbody>
           {tasks.map((task) => {
             return (
-              <tr className="bg-white border-b dark:bg-gray-900 dark:border-gray-700">
+              <tr
+                className="bg-white border-b dark:bg-gray-900 dark:border-gray-700"
+                key={task.id}
+              >
                 <th
                   scope="row"
                   className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white"
