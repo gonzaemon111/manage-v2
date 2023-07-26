5b53bf5462a0fac73baf0deac18e5f3559408eac
Gonzaemon111 <gonzaemon.engineer@gmail.com>
2023-07-26 23:31:36 +0900

    NextAuthを修正

---
+++ src/app/api/auth/[...nextauth]/authOption.ts
@@ -0,0 +1,29 @@
+import { NextAuthOptions } from "next-auth";
+import GithubProvider from "next-auth/providers/github";
+
+export const authOptions: NextAuthOptions = {
+  secret: process.env.NEXTAUTH_SECRET ?? "http://localhost:3000",
+  providers: [
+    GithubProvider({
+      clientId: process.env.GITHUB_CLIENT_ID ?? "",
+      clientSecret: process.env.GITHUB_CLIENT_SECRET ?? "",
+    }),
+  ],
+  callbacks: {
+    async jwt({ token, user, account }) {
+      // strategyがjwtの場合
+      if (user?.name) {
+        token.name = user.name;
+      }
+      return token;
+    },
+  },
+  theme: {
+    colorScheme: "auto",
+    logo:
+      process.env.NODE_ENV === "development"
+        ? "http://localhost:3000/icon.png"
+        : "/icon.png",
+    brandColor: "#5eead4",
+  },
+};
--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,24 +1,6 @@
 import NextAuth from "next-auth";
-import GithubProvider from "next-auth/providers/github";
+import { authOptions } from "./authOption";
 
-const handler = NextAuth({
-  secret: process.env.NEXTAUTH_SECRET ?? "http://localhost:3001",
-  providers: [
-    GithubProvider({
-      clientId: process.env.GITHUB_CLIENT_ID ?? "",
-      clientSecret: process.env.GITHUB_CLIENT_SECRET ?? "",
-    }),
-  ],
-  callbacks: {
-    async jwt({ token, user }) {
-      // strategyがjwtの場合
-      if (user?.name) {
-        console.log(user);
-        token.name = user.name;
-      }
-      return token;
-    },
-  },
-});
+const handler = NextAuth(authOptions);
 
 export { handler as GET, handler as POST };
