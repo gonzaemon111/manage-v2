7275eb08fda7bb15b0d7ed323f4dbd68ef7bd515
Gonzaemon111 <gonzaemon.engineer@gmail.com>
2023-07-29 12:42:04 +0900

    テストを追加

---
+++ src/components/atoms/Button/index.test.tsx
@@ -0,0 +1,34 @@
+import { composeStories } from "@storybook/react";
+import { Button, LinkButton } from ".";
+import { render } from "@testing-library/react";
+import * as stories from "./index.stories";
+
+describe("スナップショットテスト", () => {
+  const testCases = Object.values(composeStories(stories)).map((Story) => [
+    Story.storyName,
+    Story,
+  ]);
+
+  test.each(testCases)("Renders %s story", async (storyName, Story) => {
+    if (!Story) {
+      throw new Error("Story not Found");
+    }
+
+    const tree = render(<Story />);
+    expect(tree).toMatchSnapshot();
+  });
+});
+
+describe("Buttonコンポーネント", () => {
+  test("適切なclassNameが適用されている", () => {
+    const { getByText } = render(<Button name="新規作成" />);
+    expect(getByText("新規作成")).toBeTruthy();
+  });
+});
+
+describe("LinkButtonコンポーネント", () => {
+  test("適切なclassNameが適用されている", () => {
+    const { getByText } = render(<LinkButton name="新規作成" href="/" />);
+    expect(getByText("新規作成")).toBeTruthy();
+  });
+});
--- src/components/atoms/Button/index.tsx
+++ src/components/atoms/Button/index.tsx
@@ -16,7 +16,7 @@ export function Button({ name, icon }: Props) {
   return (
     <button
       type="button"
-      className="text-white bg-[#2f64e0] hover:bg-blue-500 opacity-95 hover:opacity-90 transition-all duration-200 transform focus:ring-4 focus:outline-none font-medium text-sm px-10 py-2.5 text-center shadow-md hover:shadow-xl inline-flex items-center gap-2 mr-2 mb-2"
+      className="text-white bg-primary hover:bg-blue-500 opacity-95 hover:opacity-90 transition-all duration-200 transform focus:ring-4 focus:outline-none font-medium text-sm px-10 py-2.5 text-center shadow-md hover:shadow-xl inline-flex items-center gap-2 mr-2 mb-2"
     >
       {icon}
       {name}
@@ -28,7 +28,7 @@ export function LinkButton({ name, icon, href }: LinkProps) {
   return (
     <Link
       href={href}
-      className="text-white bg-[#2f64e0] hover:bg-blue-500 opacity-95 hover:opacity-90 transition-all duration-200 transform focus:ring-4 focus:outline-none font-medium text-sm px-10 py-2.5 text-center shadow-md hover:shadow-xl inline-flex items-center mr-2 mb-2"
+      className="text-white bg-primary hover:bg-blue-500 opacity-95 hover:opacity-90 transition-all duration-200 transform focus:ring-4 focus:outline-none font-medium text-sm px-10 py-2.5 text-center shadow-md hover:shadow-xl inline-flex items-center mr-2 mb-2"
     >
       {icon}
       {name}
