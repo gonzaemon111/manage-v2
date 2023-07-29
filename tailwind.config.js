ebd4616441ba8fca52d0b4f17c7a54c058f2b1a5
Gonzaemon111 <gonzaemon.engineer@gmail.com>
2023-07-29 11:41:09 +0900

    設定周りを修正

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,17 @@ module.exports = {
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
+    colors: {
+      transparent: "transparent",
+      current: "currentColor",
+      midnight: "#121063",
+      metal: "#565584",
+      tahiti: "#3ab7bf",
+      silver: "#ecebff",
+      "bubble-gum": "#ff77e9",
+      bermuda: "#78dcca",
+      primary: "#2f64e0",
+    },
     extend: {
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
