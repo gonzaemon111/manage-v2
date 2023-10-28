b9255173a5081be7ba7835a57392964804fa65c6
Gonzaemon111 <gonzaemon.engineer@gmail.com>
2023-10-29 01:32:46 +0900

    環境変数を変更

--- src/app/api/auth/[...nextauth]/authOption.ts
+++ src/app/api/auth/[...nextauth]/authOption.ts
@@ -5,8 +5,8 @@ export const authOptions: NextAuthOptions = {
   secret: process.env.NEXTAUTH_SECRET ?? "http://localhost:3000",
   providers: [
     GithubProvider({
-      clientId: process.env.GITHUB_CLIENT_ID ?? "",
-      clientSecret: process.env.GITHUB_CLIENT_SECRET ?? "",
+      clientId: process.env.NEXTAUTH_GITHUB_CLIENT_ID ?? "",
+      clientSecret: process.env.NEXTAUTH_GITHUB_CLIENT_SECRET ?? "",
     }),
   ],
   callbacks: {
