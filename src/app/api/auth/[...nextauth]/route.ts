6878abcc33a19fce4211608292e4296645d01469
Gonzaemon111 <gonzaemon.engineer@gmail.com>
2023-11-09 08:11:16 +0900

    🛡️ NextAuthの設定を変更 close #157

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,5 +1,5 @@
-import NextAuth from "next-auth";
-import { authOptions } from "./authOption";
+import NextAuth from 'next-auth';
+import { authOptions } from './authOption';
 
 const handler = NextAuth(authOptions);
 
