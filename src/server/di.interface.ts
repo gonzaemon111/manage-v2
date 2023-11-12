d717109ddbcc0917fb69c6eae63999792054437d
Gonzaemon111 <gonzaemon.engineer@gmail.com>
2023-11-12 11:38:59 +0900

    🐺 SubscriptionRepositoryを追加 close #167

--- src/server/di.interface.ts
+++ src/server/di.interface.ts
@@ -1,6 +1,10 @@
 export { type DomainRepository } from '@/server/domain/Domain';
 export { type UserRepository } from '@/server/domain/User';
+export { type SubscriptionRepository } from '@/server/domain/Subscription';
 export * from '@/server/domain/Task';
+
+// Service
+export * from '@/server/infrastructure/repository';
 
 // Service
 export * from '@/server/application/service';
