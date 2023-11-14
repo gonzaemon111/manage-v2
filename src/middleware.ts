fdc6151694b42c57bc3d515cf3f840646065b202
G.S <gonzaemon.engineer@gmail.com>
2023-11-15 02:33:52 +0900

    🔬 Middlewareを修正 close #176 (#177)

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,5 +1,5 @@
-export { default } from "next-auth/middleware";
+export { default } from 'next-auth/middleware';
 
 export const config = {
-  matcher: ["/^/(apps|api/apps)(/|$)/"],
+  matcher: ['/((?!api/health|icon.png).*)']
 };
