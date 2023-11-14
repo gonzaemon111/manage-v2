04cded93235b075ad7bcd422320045562c11f1e3
G.S <gonzaemon.engineer@gmail.com>
2023-11-15 02:46:02 +0900

    🎖️ SidebarにSubscriptionを追加 close #178 (#179)

--- src/components/organisms/Sidebar/index.tsx
+++ src/components/organisms/Sidebar/index.tsx
@@ -1,4 +1,4 @@
-import Link from "next/link";
+import Link from 'next/link';
 
 interface Props {
   readonly sidebarOpen: boolean;
@@ -8,137 +8,153 @@ export function Sidebar({ sidebarOpen = true }: Props) {
   return (
     <aside
       className={`shrink-0 flex flex-col w-64 pl-5 pr-3 py-8 overflow-y-auto bg-slate-600 dark:bg-slate-800 ${
-        sidebarOpen ? "block" : "hidden"
+        sidebarOpen ? 'block' : 'hidden'
       }`}
     >
-      <div className="flex flex-col justify-between flex-1">
-        <nav className="-mx-3 space-y-6 ">
-          <div className="space-y-3 ">
-            <label className="px-3 text-slate-100 uppercase">
-              プライベート
-            </label>
+      <div className='flex flex-col justify-between flex-1'>
+        <nav className='-mx-3 space-y-6 '>
+          <div className='space-y-3 '>
+            <label className='px-3 text-slate-100 uppercase'>プライベート</label>
 
             <Link
-              className="flex items-center px-3 py-2 text-slate-400 transition-colors duration-300 transform hover:border-r-4 hover:border-slate-200 hover:text-slate-200 hover:bg-slate-500"
-              href="/tasks"
+              className='flex items-center px-3 py-2 text-slate-400 transition-colors duration-300 transform hover:border-r-4 hover:border-slate-200 hover:text-slate-200 hover:bg-slate-500'
+              href='/tasks'
             >
               <svg
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 24 24"
+                xmlns='http://www.w3.org/2000/svg'
+                fill='none'
+                viewBox='0 0 24 24'
                 strokeWidth={1.5}
-                stroke="currentColor"
-                className="w-5 h-5"
+                stroke='currentColor'
+                className='w-5 h-5'
               >
                 <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  d="M9 12.75L11.25 15 15 9.75m-3-7.036A11.959 11.959 0 013.598 6 11.99 11.99 0 003 9.749c0 5.592 3.824 10.29 9 11.623 5.176-1.332 9-6.03 9-11.622 0-1.31-.21-2.571-.598-3.751h-.152c-3.196 0-6.1-1.248-8.25-3.285z"
+                  strokeLinecap='round'
+                  strokeLinejoin='round'
+                  d='M9 12.75L11.25 15 15 9.75m-3-7.036A11.959 11.959 0 013.598 6 11.99 11.99 0 003 9.749c0 5.592 3.824 10.29 9 11.623 5.176-1.332 9-6.03 9-11.622 0-1.31-.21-2.571-.598-3.751h-.152c-3.196 0-6.1-1.248-8.25-3.285z'
                 />
               </svg>
 
-              <span className="mx-2 text-sm font-medium">タスク</span>
+              <span className='mx-2 text-sm font-medium'>タスク</span>
             </Link>
           </div>
 
-          <div className="space-y-3 ">
-            <label className="px-3 text-slate-100 uppercase">在庫管理</label>
+          <div className='space-y-3 '>
+            <label className='px-3 text-slate-100 uppercase'>在庫管理</label>
 
             <Link
-              className="flex items-center px-3 py-2 text-slate-400 transition-colors duration-300 transform hover:border-r-4 hover:text-slate-200 hover:bg-slate-500"
-              href="#"
+              className='flex items-center px-3 py-2 text-slate-400 transition-colors duration-300 transform hover:border-r-4 hover:text-slate-200 hover:bg-slate-500'
+              href='#'
             >
               <svg
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 24 24"
+                xmlns='http://www.w3.org/2000/svg'
+                fill='none'
+                viewBox='0 0 24 24'
                 strokeWidth={1.5}
-                stroke="currentColor"
-                className="w-5 h-5"
+                stroke='currentColor'
+                className='w-5 h-5'
               >
                 <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  d="M20.25 7.5l-.625 10.632a2.25 2.25 0 01-2.247 2.118H6.622a2.25 2.25 0 01-2.247-2.118L3.75 7.5M10 11.25h4M3.375 7.5h17.25c.621 0 1.125-.504 1.125-1.125v-1.5c0-.621-.504-1.125-1.125-1.125H3.375c-.621 0-1.125.504-1.125 1.125v1.5c0 .621.504 1.125 1.125 1.125z"
+                  strokeLinecap='round'
+                  strokeLinejoin='round'
+                  d='M20.25 7.5l-.625 10.632a2.25 2.25 0 01-2.247 2.118H6.622a2.25 2.25 0 01-2.247-2.118L3.75 7.5M10 11.25h4M3.375 7.5h17.25c.621 0 1.125-.504 1.125-1.125v-1.5c0-.621-.504-1.125-1.125-1.125H3.375c-.621 0-1.125.504-1.125 1.125v1.5c0 .621.504 1.125 1.125 1.125z'
                 />
               </svg>
 
-              <span className="mx-2 text-sm font-medium">日用品</span>
+              <span className='mx-2 text-sm font-medium'>日用品</span>
             </Link>
 
             <Link
-              className="flex items-center px-3 py-2 text-slate-400 transition-colors duration-300 transform hover:border-r-4 hover:text-slate-200 hover:bg-slate-500"
-              href="/domains"
+              className='flex items-center px-3 py-2 text-slate-400 transition-colors duration-300 transform hover:border-r-4 hover:text-slate-200 hover:bg-slate-500'
+              href='/domains'
             >
               <svg
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 24 24"
-                strokeWidth="1.5"
-                stroke="currentColor"
-                className="w-5 h-5"
+                xmlns='http://www.w3.org/2000/svg'
+                fill='none'
+                viewBox='0 0 24 24'
+                strokeWidth='1.5'
+                stroke='currentColor'
+                className='w-5 h-5'
               >
                 <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  d="M12 21a9.004 9.004 0 008.716-6.747M12 21a9.004 9.004 0 01-8.716-6.747M12 21c2.485 0 4.5-4.03 4.5-9S14.485 3 12 3m0 18c-2.485 0-4.5-4.03-4.5-9S9.515 3 12 3m0 0a8.997 8.997 0 017.843 4.582M12 3a8.997 8.997 0 00-7.843 4.582m15.686 0A11.953 11.953 0 0112 10.5c-2.998 0-5.74-1.1-7.843-2.918m15.686 0A8.959 8.959 0 0121 12c0 .778-.099 1.533-.284 2.253m0 0A17.919 17.919 0 0112 16.5c-3.162 0-6.133-.815-8.716-2.247m0 0A9.015 9.015 0 013 12c0-1.605.42-3.113 1.157-4.418"
+                  strokeLinecap='round'
+                  strokeLinejoin='round'
+                  d='M12 21a9.004 9.004 0 008.716-6.747M12 21a9.004 9.004 0 01-8.716-6.747M12 21c2.485 0 4.5-4.03 4.5-9S14.485 3 12 3m0 18c-2.485 0-4.5-4.03-4.5-9S9.515 3 12 3m0 0a8.997 8.997 0 017.843 4.582M12 3a8.997 8.997 0 00-7.843 4.582m15.686 0A11.953 11.953 0 0112 10.5c-2.998 0-5.74-1.1-7.843-2.918m15.686 0A8.959 8.959 0 0121 12c0 .778-.099 1.533-.284 2.253m0 0A17.919 17.919 0 0112 16.5c-3.162 0-6.133-.815-8.716-2.247m0 0A9.015 9.015 0 013 12c0-1.605.42-3.113 1.157-4.418'
                 />
               </svg>
 
-              <span className="mx-2 text-sm font-medium">ドメイン</span>
+              <span className='mx-2 text-sm font-medium'>ドメイン</span>
+            </Link>
+
+            <Link
+              className='flex items-center px-3 py-2 text-slate-400 transition-colors duration-300 transform hover:border-r-4 hover:text-slate-200 hover:bg-slate-500'
+              href='/subscriptions'
+            >
+              <svg
+                xmlns='http://www.w3.org/2000/svg'
+                fill='none'
+                viewBox='0 0 24 24'
+                strokeWidth={1.5}
+                stroke='currentColor'
+                className='w-5 h-5'
+              >
+                <path
+                  strokeLinecap='round'
+                  strokeLinejoin='round'
+                  d='M9 9l10.5-3m0 6.553v3.75a2.25 2.25 0 01-1.632 2.163l-1.32.377a1.803 1.803 0 11-.99-3.467l2.31-.66a2.25 2.25 0 001.632-2.163zm0 0V2.25L9 5.25v10.303m0 0v3.75a2.25 2.25 0 01-1.632 2.163l-1.32.377a1.803 1.803 0 01-.99-3.467l2.31-.66A2.25 2.25 0 009 15.553z'
+                />
+              </svg>
+
+              <span className='mx-2 text-sm font-medium'>サブスクリプション</span>
             </Link>
           </div>
 
-          <div className="space-y-3">
-            <label className="px-3 text-slate-100 uppercase">設定</label>
+          <div className='space-y-3'>
+            <label className='px-3 text-slate-100 uppercase'>設定</label>
 
             <Link
-              className="flex items-center px-3 py-2 text-slate-400 transition-colors duration-300 transform hover:border-r-4 hover:text-slate-200 hover:bg-slate-500"
-              href="#"
+              className='flex items-center px-3 py-2 text-slate-400 transition-colors duration-300 transform hover:border-r-4 hover:text-slate-200 hover:bg-slate-500'
+              href='#'
             >
               <svg
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 24 24"
-                strokeWidth="1.5"
-                stroke="currentColor"
-                className="w-5 h-5"
+                xmlns='http://www.w3.org/2000/svg'
+                fill='none'
+                viewBox='0 0 24 24'
+                strokeWidth='1.5'
+                stroke='currentColor'
+                className='w-5 h-5'
               >
                 <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  d="M4.098 19.902a3.75 3.75 0 005.304 0l6.401-6.402M6.75 21A3.75 3.75 0 013 17.25V4.125C3 3.504 3.504 3 4.125 3h5.25c.621 0 1.125.504 1.125 1.125v4.072M6.75 21a3.75 3.75 0 003.75-3.75V8.197M6.75 21h13.125c.621 0 1.125-.504 1.125-1.125v-5.25c0-.621-.504-1.125-1.125-1.125h-4.072M10.5 8.197l2.88-2.88c.438-.439 1.15-.439 1.59 0l3.712 3.713c.44.44.44 1.152 0 1.59l-2.879 2.88M6.75 17.25h.008v.008H6.75v-.008z"
+                  strokeLinecap='round'
+                  strokeLinejoin='round'
+                  d='M4.098 19.902a3.75 3.75 0 005.304 0l6.401-6.402M6.75 21A3.75 3.75 0 013 17.25V4.125C3 3.504 3.504 3 4.125 3h5.25c.621 0 1.125.504 1.125 1.125v4.072M6.75 21a3.75 3.75 0 003.75-3.75V8.197M6.75 21h13.125c.621 0 1.125-.504 1.125-1.125v-5.25c0-.621-.504-1.125-1.125-1.125h-4.072M10.5 8.197l2.88-2.88c.438-.439 1.15-.439 1.59 0l3.712 3.713c.44.44.44 1.152 0 1.59l-2.879 2.88M6.75 17.25h.008v.008H6.75v-.008z'
                 />
               </svg>
 
-              <span className="mx-2 text-sm font-medium">テーマ</span>
+              <span className='mx-2 text-sm font-medium'>テーマ</span>
             </Link>
 
             <Link
-              className="flex items-center px-3 py-2 text-slate-400 transition-colors duration-300 transform hover:border-r-4 hover:text-slate-200 hover:bg-slate-500"
-              href="#"
+              className='flex items-center px-3 py-2 text-slate-400 transition-colors duration-300 transform hover:border-r-4 hover:text-slate-200 hover:bg-slate-500'
+              href='#'
             >
               <svg
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 24 24"
-                strokeWidth="1.5"
-                stroke="currentColor"
-                className="w-5 h-5"
+                xmlns='http://www.w3.org/2000/svg'
+                fill='none'
+                viewBox='0 0 24 24'
+                strokeWidth='1.5'
+                stroke='currentColor'
+                className='w-5 h-5'
               >
                 <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  d="M10.343 3.94c.09-.542.56-.94 1.11-.94h1.093c.55 0 1.02.398 1.11.94l.149.894c.07.424.384.764.78.93.398.164.855.142 1.205-.108l.737-.527a1.125 1.125 0 011.45.12l.773.774c.39.389.44 1.002.12 1.45l-.527.737c-.25.35-.272.806-.107 1.204.165.397.505.71.93.78l.893.15c.543.09.94.56.94 1.109v1.094c0 .55-.397 1.02-.94 1.11l-.893.149c-.425.07-.765.383-.93.78-.165.398-.143.854.107 1.204l.527.738c.32.447.269 1.06-.12 1.45l-.774.773a1.125 1.125 0 01-1.449.12l-.738-.527c-.35-.25-.806-.272-1.203-.107-.397.165-.71.505-.781.929l-.149.894c-.09.542-.56.94-1.11.94h-1.094c-.55 0-1.019-.398-1.11-.94l-.148-.894c-.071-.424-.384-.764-.781-.93-.398-.164-.854-.142-1.204.108l-.738.527c-.447.32-1.06.269-1.45-.12l-.773-.774a1.125 1.125 0 01-.12-1.45l.527-.737c.25-.35.273-.806.108-1.204-.165-.397-.505-.71-.93-.78l-.894-.15c-.542-.09-.94-.56-.94-1.109v-1.094c0-.55.398-1.02.94-1.11l.894-.149c.424-.07.765-.383.93-.78.165-.398.143-.854-.107-1.204l-.527-.738a1.125 1.125 0 01.12-1.45l.773-.773a1.125 1.125 0 011.45-.12l.737.527c.35.25.807.272 1.204.107.397-.165.71-.505.78-.929l.15-.894z"
+                  strokeLinecap='round'
+                  strokeLinejoin='round'
+                  d='M10.343 3.94c.09-.542.56-.94 1.11-.94h1.093c.55 0 1.02.398 1.11.94l.149.894c.07.424.384.764.78.93.398.164.855.142 1.205-.108l.737-.527a1.125 1.125 0 011.45.12l.773.774c.39.389.44 1.002.12 1.45l-.527.737c-.25.35-.272.806-.107 1.204.165.397.505.71.93.78l.893.15c.543.09.94.56.94 1.109v1.094c0 .55-.397 1.02-.94 1.11l-.893.149c-.425.07-.765.383-.93.78-.165.398-.143.854.107 1.204l.527.738c.32.447.269 1.06-.12 1.45l-.774.773a1.125 1.125 0 01-1.449.12l-.738-.527c-.35-.25-.806-.272-1.203-.107-.397.165-.71.505-.781.929l-.149.894c-.09.542-.56.94-1.11.94h-1.094c-.55 0-1.019-.398-1.11-.94l-.148-.894c-.071-.424-.384-.764-.781-.93-.398-.164-.854-.142-1.204.108l-.738.527c-.447.32-1.06.269-1.45-.12l-.773-.774a1.125 1.125 0 01-.12-1.45l.527-.737c.25-.35.273-.806.108-1.204-.165-.397-.505-.71-.93-.78l-.894-.15c-.542-.09-.94-.56-.94-1.109v-1.094c0-.55.398-1.02.94-1.11l.894-.149c.424-.07.765-.383.93-.78.165-.398.143-.854-.107-1.204l-.527-.738a1.125 1.125 0 01.12-1.45l.773-.773a1.125 1.125 0 011.45-.12l.737.527c.35.25.807.272 1.204.107.397-.165.71-.505.78-.929l.15-.894z'
                 />
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  d="M15 12a3 3 0 11-6 0 3 3 0 016 0z"
-                />
+                <path strokeLinecap='round' strokeLinejoin='round' d='M15 12a3 3 0 11-6 0 3 3 0 016 0z' />
               </svg>
 
-              <span className="mx-2 text-sm font-medium">個人設定</span>
+              <span className='mx-2 text-sm font-medium'>個人設定</span>
             </Link>
           </div>
         </nav>
