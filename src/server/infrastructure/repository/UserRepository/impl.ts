30893ee5dcd6eb2608ee6229d87783a838dd2ab1
G.S <gonzaemon.engineer@gmail.com>
2023-11-17 02:42:22 +0900

    🔴 Repositoryを一部リファクタ close #180 (#181)

--- src/server/infrastructure/repository/UserRepository/impl.ts
+++ src/server/infrastructure/repository/UserRepository/impl.ts
@@ -1,3 +1,4 @@
+import 'reflect-metadata';
 import { injectable } from 'inversify';
 import { InternalServerError } from '@/server/domain/Errors/InternalServerError';
 import { NotFoundError } from '@/server/domain/Errors/NotFoundError';
